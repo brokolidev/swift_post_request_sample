8d6b752d383f40278db5a9a0d37bd252bd534f53
brokolidev <cto@planitcorp.kr>
2020-04-20 11:42:09 +0900

    create network manager class to send post request

--- postRequestSample/ContentView.swift
+++ postRequestSample/ContentView.swift
@@ -7,11 +7,43 @@
 //
 
 import SwiftUI
+import Combine
+
+class HttpAuth: ObservableObject {
+    var didChange = PassthroughSubject<HttpAuth, Never>()
+    
+    var authenticated = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    func checkDetails(username: String, password: String) {
+        guard let url = URL(string: "https://api.boadream.co.kr/api/login") else { return }
+        
+        let body: [String: String] = ["mb_id": username, "password": password]
+        
+        let finalBody = try! JSONSerialization.data(withJSONObject: body)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalBody
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(data)
+        }.resume()
+        
+    }
+}
 
 struct ContentView: View {
     
     @State private var username: String = ""
     @State private var password: String = ""
+    
+    var manager = HttpAuth()
     
     var body: some View {
         
@@ -24,8 +56,9 @@ struct ContentView: View {
             
             
             Button(action: {
-//                print("clicked")
                 print("\(self.username) and \(self.password)")
+                
+                self.manager.checkDetails(username: self.username, password: self.password)
             }) {
                 Text("Login")
             }
