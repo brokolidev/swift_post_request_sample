aa127100a9316ada364bf4fd9b8f38c033f16e4f
brokolidev <cto@planitcorp.kr>
2020-04-21 14:28:01 +0900

    add myinfoview items

--- postRequestSample/ContentView.swift
+++ postRequestSample/ContentView.swift
@@ -9,6 +9,16 @@
 import SwiftUI
 import Combine
 
+extension DateFormatter {
+  static let iso8601Full: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    return formatter
+  }()
+}
 
 
 struct User: Codable {
@@ -67,15 +77,10 @@ class HttpAuth: ObservableObject {
                     let paymentData = Data(userInfo.mb10.utf8)
                     let paymentInfo = try! decoder.decode(PaymentInfo.self, from: paymentData)
                     
-                    print(paymentInfo.RD)
+                    print(paymentInfo)
                     
                 }
             }
-            
-            
-            
-            
-            
             
         }.resume()
         
